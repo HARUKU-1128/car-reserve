1a4a4bc7d27e59c39fae6227d384cdbf93610e58
HARUKU-1128 <haruku.kagawa@gmail.com>
2025-08-10 22:01:19 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'car-reserve-cache-v3-7';
+const CACHE_NAME = 'car-reserve-cache-v3-8';
 const ASSETS = [
   './', './index.html', './style.css', './app.js',
   './manifest.json', './icons/icon-192.png', './icons/icon-512.png'
